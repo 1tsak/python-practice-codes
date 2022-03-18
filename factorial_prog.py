6ddffa4ec477fe4c38d58f2898ecd4e0f4e45385
A2v10 <hey.a2v10@gmail.com>
2022-03-18 22:12:25 +0530

    Initial Commit Adding 3 Python Programs

---
+++ factorial_prog.py
@@ -0,0 +1,7 @@
+print("/////////////////////Factorial Program////////////////////////")
+n=int(input("Enter the number: "))
+factorial =1
+for i in range(1,n): 
+    factorial=factorial*i
+
+print("The factorial is: ",factorial)
---
+++ sum_prog.py
@@ -0,0 +1,5 @@
+print("Calculate Sum:/n")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = a+b
+print("The Sum is: ",c) 
